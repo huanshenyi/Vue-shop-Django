517bd714265ac112a2236733077ed4614aca263a
huanshenyi <txy1226052@gmail.com>
2019-07-09 21:58:25 +0900

    ModelSerializerの使い方

--- apps/goods/serializers.py
+++ apps/goods/serializers.py
@@ -1,7 +1,23 @@
 from rest_framework import serializers
 
+from goods.models import Goods, GoodsCategory
 
-class GoodsSerializer(serializers.Serializer):
-    name = serializers.CharField(required=True, max_length=100)
-    click_num = serializers.IntegerField(default=0)
-    goods_front_image = serializers.ImageField()
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GoodsCategory
+        fields = "__all__"
+
+
+class GoodsSerializer(serializers.ModelSerializer):
+    # name = serializers.CharField(required=True, max_length=100)
+    # click_num = serializers.IntegerField(default=0)
+    # goods_front_image = serializers.ImageField()
+
+    # def create(self, validated_data):
+    #     return Goods.objects.create(**validated_data)
+    category = CategorySerializer()
+    class Meta:
+        model = Goods
+        fields = "__all__"
+        # fields = ('name', 'click_num', 'market_price', 'add_time')
--- apps/users/models.py
+++ apps/users/models.py
@@ -22,10 +22,7 @@ class UserProfile(AbstractUser):
         verbose_name_plural = verbose_name
 
     def __str__(self):
-       if(self.name==None):
-           return ""
-       else:
-           return self.name
+       return self.username
 
 
 class VerifyCode(models.Model):
